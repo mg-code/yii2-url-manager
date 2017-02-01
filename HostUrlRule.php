2c66ae74b13375477930c5d2af897c7f95c1a5d7
Matrozov <me@mear.me>
2017-02-01 23:08:02 +0200

    Add verb support

--- HostUrlRule.php
+++ HostUrlRule.php
@@ -5,6 +5,7 @@ namespace mgcode\urlManager;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\web\CompositeUrlRule;
+use yii\web\UrlRule;
 use yii\web\UrlRuleInterface;
 
 /**
@@ -50,21 +51,30 @@ class HostUrlRule extends CompositeUrlRule
      */
     protected function createRules()
     {
-        $rules = [];
+        $compiledRules = [];
+        $verbs = 'GET|HEAD|POST|PUT|PATCH|DELETE|OPTIONS';
         foreach ($this->rules as $key => $rule) {
-            if (!is_array($rule)) {
-                $rule = [
-                    'pattern' => $key,
-                    'route' => $rule,
-                ];
+            if (is_string($rule)) {
+                $rule = ['route' => $rule];
+                if (preg_match("/^((?:($verbs),)*($verbs))\\s+(.*)$/", $key, $matches)) {
+                    $rule['verb'] = explode(',', $matches[1]);
+                    // rules that do not apply for GET requests should not be use to create urls
+                    if (!in_array('GET', $rule['verb'])) {
+                        $rule['mode'] = UrlRule::PARSING_ONLY;
+                    }
+                    $key = $matches[4];
+                }
+                $rule['pattern'] = $key;
             }
-            $rule = Yii::createObject(array_merge($this->ruleConfig, $rule));
+            if (is_array($rule)) {
+                $rule = Yii::createObject(array_merge($this->ruleConfig, $rule));
+            }
             if (!$rule instanceof UrlRuleInterface) {
                 throw new InvalidConfigException('URL rule class must implement UrlRuleInterface.');
             }
-            $rules[] = $rule;
+            $compiledRules[] = $rule;
         }
-        return $rules;
+        return $compiledRules;
     }
 
     /**
